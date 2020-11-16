57b57143efbe6cda98941bf1aa46b5437985a071
ivan-imd <ivanlfsilva@ufrn.edu.br>
2020-11-15 22:40:13 -0300

    respostas

--- aula03/Atividade_03_2-html/Atividade_03_2.js
+++ aula03/Atividade_03_2-html/Atividade_03_2.js
@@ -7,8 +7,11 @@
 //var valor_total    = 3;
 //document.getElementById("saida").innerHTML = valor_total;
 
+/*
 //var 1_valor_total  = 4;
 //document.getElementById("saida").innerHTML = 1_valor_total;
+Números também não podem inicia identificadores em javascript
+/*
 
 //var valor_total_1  = 5;
 //document.getElementById("saida").innerHTML = valor_total_1;
@@ -19,11 +22,15 @@
 //var _valor_total   = 7;
 //document.getElementById("saida").innerHTML = _valor_total;
 
-//var valor-total    = 8;
-//document.getElementById("saida").innerHTML = valor-total;
+/*
+var valor-total    = 8;
+document.getElementById("saida").innerHTML = valor-total;
+o hifen também não pode ser usado como indentificador
+/*
 
-//var function       = 9;
+//var function      = 9;
 //document.getElementById("saida").innerHTML = function;
+// O indentificador function é uma palavra reservada do javascript, por isso ocasiona uma erro.
 
 //var object       = 9;
 //document.getElementById("saida").innerHTML = object;
