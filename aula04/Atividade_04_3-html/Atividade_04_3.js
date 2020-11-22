4b1b47aca160c3922c628cd20d69cba57ba4f45d
ivan-imd <ivanlfsilva@ufrn.edu.br>
2020-11-22 14:06:30 -0300

    add

--- aula04/Atividade_04_3-html/Atividade_04_3.js
+++ aula04/Atividade_04_3-html/Atividade_04_3.js
@@ -3,11 +3,19 @@ function positivos() {
 }
 
 function ler_entrada(campo) {
+    
+    var a = Number(document.getElementById("entrada1").value)
+    var b = Number(document.getElementById("entrada2").value)
+    if( a > 0 == true && b > 0 == true){
+        document.getElementById("saida").innerHTML = "true"
+    }else{
+        document.getElementById("saida").innerHTML = "false"
+    }
 
 }
 
 function escrever_saida(campo, valor) {
-
+    
 }
 
 function maior(valor, limiar) {
--- aula04/Atividade_04_4-html/Atividade_04_4.js
+++ aula04/Atividade_04_4-html/Atividade_04_4.js
@@ -1,2 +1,14 @@
 function pitagoras() {
+  var hipo =  Number(document.getElementById("hipotenusa").value)
+  var cat1 =  Number(document.getElementById("cateto1").value)    
+  var cat2 =  Number(document.getElementById("cateto2").value)
+
+  if(cat1**2 + cat2**2 == hipo**2){
+
+    document.getElementById("saida").innerHTML = "true"
+
+  }else{
+      document.getElementById("saida").innerHTML = "false"
+  }
+
 }
